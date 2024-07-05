347606503b3b0ad51d1f8c3485e44183ac8c37aa
Alison549 <ali.samu65@gmail.com>
2024-07-04 20:59:29 -0500

    Estructura style y JavaScripts
    Complemento css, image y javascripts

---
+++ Js/scripts.js
@@ -0,0 +1,10 @@
+//EVENTOS DE SCRIPTS
+const datos = {
+    nombre: '',
+    telefono: '',
+    email: '',
+}
+
+let nombre = document.querySelector('#nombre');
+let telefono = document.querySelector('#telefono');
+let email = document.querySelector('#email');
